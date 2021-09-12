2b5b857adcf3d3b4a8e7619bcfbcd42980a42338
fernando-utiyama <fernandoutiyama@gmail.com>
2021-09-12 11:17:36 -0300

    Delete all API

--- src/main/java/com/univesp/pi6backend/controller/ProductResource.java
+++ src/main/java/com/univesp/pi6backend/controller/ProductResource.java
@@ -5,12 +5,15 @@ import com.univesp.pi6backend.repository.ProductEnum;
 import com.univesp.pi6backend.repository.ProductJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -51,6 +54,12 @@ public class ProductResource {
         productJpaRepository.save(entity);
         URI uri = uriBuilder.path("/products/product/?id={id}").buildAndExpand(entity.getId()).toUri();
         return ResponseEntity.created(uri).body(entity);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/all")
+    public void deleteAllProducts() {
+        productJpaRepository.deleteAll();
     }
 
 }

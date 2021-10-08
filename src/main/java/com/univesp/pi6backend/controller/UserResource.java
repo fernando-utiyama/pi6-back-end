85b0ba82159c203395f2e43d0ec46a75bc3d71f3
fernando-utiyama <fernandoutiyama@gmail.com>
2021-10-08 13:58:57 -0300

    Database Refactor

--- src/main/java/com/univesp/pi6backend/controller/UserResource.java
+++ src/main/java/com/univesp/pi6backend/controller/UserResource.java
@@ -4,8 +4,11 @@ import com.univesp.pi6backend.repository.Usuario;
 import com.univesp.pi6backend.repository.UsuarioJpaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +25,11 @@ public class UserResource {
     public List<Usuario> getAllUsers() {
         return usuarioJpaRepository.findAll();
     }
+
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/all")
+    public void deleteAllUsers() {
+        usuarioJpaRepository.deleteAll();
+    }
+
 }
